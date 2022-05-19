65ac3cfb909c9ef965dbb4a2bb22f7a31dc76004
AndreyShulgin <neonod404@gmail.com>
2022-05-19 23:29:47 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Alex");
+        student.setGroup("4-B");
+        student.setDate("12 September");
+        String ln = System.lineSeparator();
+        System.out.println("Name : " + student.getName() + ln +
+                "Group : " + student.getGroup() + ln +
+                "Date : " + student.getDate());
+    }
+}
