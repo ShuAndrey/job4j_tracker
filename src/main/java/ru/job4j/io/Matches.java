65059f2d3e925de0b383037c0976d24c46ffb199
Andrey Shulgin <neonod404@yandex.ru>
2022-05-23 13:40:03 +0300

    2. Scanner и чтение числа из консоли. [#179518 #304018]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -10,21 +10,24 @@ public class Matches {
         int count = 11;
         while (count > 0) {
             String player = turn ? "Первый игрок" : "Второй игрок";
-            System.out.println(player + " введите число от 1 до 3:");
+            if (count == 2) {
+                System.out.println(player + " введите число от 1 до 2:");
+            } else if (count == 1) {
+                System.out.println(player + " введите число 1:");
+            } else {
+                System.out.println(player + " введите число от 1 до 3:");
+            }
             int matches = Integer.parseInt(input.nextLine());
-            while (matches < 1 || matches > 3) {
-                System.out.println("Число не 1 - 3, повторите ввод.");
-                matches = Integer.parseInt(input.nextLine());
-            }
-            count -= matches;
-            while (count < 0) {
-                System.out.println("На столе спичек меньше чем вы хотите взять, повторите попытку.");
-                count += matches;
-                matches = Integer.parseInt(input.nextLine());
+            int temp = count - matches;
+            if (matches >= 1 && matches <= 3 && temp >= 0) {
                 count -= matches;
+                System.out.println("Осталось : " + count);
+                turn = !turn;
+            } else if (temp < 0) {
+                System.out.println("Вы пытаетесь ввести число, которое превышает остаток равный " + count);
+            } else {
+                System.out.println("Введенное число не 1 - 3. Повторите попытку.");
             }
-            System.out.println("Осталось : " + count);
-            turn = !turn;
         }
         if (!turn) {
             System.out.println("Выиграл первый игрок");
