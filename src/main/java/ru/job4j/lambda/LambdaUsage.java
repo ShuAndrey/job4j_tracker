f54d368f031cf3249a59bac8af9e1b26df22d7ec
Andrey Shulgin <neonod404@yandex.ru>
2022-06-08 11:21:48 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,30 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Класс демонстрирует применение лямбда блока.
+ *
+ * @author Andrey Shulgin
+ */
+public class LambdaUsage {
+
+    /**
+     * Метод демонстрирует применение лямбда блока.
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return Integer.compare(right.length(), left.length());
+        };
+        strings.sort(comparator);
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+}
