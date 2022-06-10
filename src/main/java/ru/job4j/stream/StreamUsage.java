d19ecc8fdb67422106c466a7cfb01f245ec561c5
AndreyShulgin <neonod404@gmail.com>
2022-06-10 23:06:56 +0300

    0.1. Отладка лямбда в IDEA [#259165]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,41 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+/**
+ * Класс демонстрирует возможности Stream API.
+ *
+ * @author Andrey Shulgin
+ */
+public class StreamUsage {
+    /**
+     * Модель данных.
+     */
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+    }
+
+    /**
+     * Метод выводит на консоль task содержащий Bug и spent > 30.
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
+    }
+}
