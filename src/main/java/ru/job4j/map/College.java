c136c5827af581d78ef5521563124b83b5d9e679
AndreyShulgin <neonod404@gmail.com>
2022-06-11 22:13:21 +0300

    3. Optional в банковских переводах. [#214711]

---
+++ src/main/java/ru/job4j/map/College.java
@@ -0,0 +1,57 @@
+package ru.job4j.map;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Класс с методами поиска студента и предмета.
+ *
+ * @author Andrey Shulgin
+ */
+public class College {
+    private final Map<Student, Set<Subject>> students;
+
+    public College(Map<Student, Set<Subject>> students) {
+        this.students = students;
+    }
+
+    /**
+     * Метод поиска студента по аккаунту.
+     *
+     * @param account - аккаунт.
+     * @return - студент.
+     */
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl = Optional.empty();
+        for (Student s : students.keySet()) {
+            if (account.equals(s.getAccount())) {
+                rsl = Optional.of(s);
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод поиска предмета по аккаунту и имени.
+     *
+     * @param account - аккаунт.
+     * @param name    - имя.
+     * @return - предмет.
+     */
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Subject> rsl = Optional.empty();
+        Optional<Student> s = findByAccount(account);
+        if (s.isPresent()) {
+            Set<Subject> subjects = students.get(s.get());
+            for (Subject subj : subjects) {
+                if (name.equals(subj.getName())) {
+                    rsl = Optional.of(subj);
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+}
