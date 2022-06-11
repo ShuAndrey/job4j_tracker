55ae7c8c32d4b4cc30da3885227d4c19f2b99e28
AndreyShulgin <neonod404@gmail.com>
2022-06-11 14:43:53 +0300

    8. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/stream/Suit.java
@@ -0,0 +1,10 @@
+package ru.job4j.stream;
+
+/**
+ * Масти карт.
+ *
+ * @author Andrey Shulgin
+ */
+public enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
---
+++ src/main/java/ru/job4j/stream/Value.java
@@ -0,0 +1,10 @@
+package ru.job4j.stream;
+
+/**
+ * Значения карт.
+ *
+ * @author Andrey Shulgin
+ */
+public enum Value {
+    V_6, V_7, V_8
+}
